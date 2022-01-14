5a3b5971b02797b8800d1fe35c473a123587e27c
Pedja Stojiljkovic <pedja1310@gmail.com>
2022-01-14 15:58:27 +0100

    updated names of the pages in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,14 @@ import { Container } from "react-bootstrap";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Auth from "./pages/Auth";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Products from "./pages/Products";
-import ProductPage from "./pages/ProductPage";
+import AuthPage from "./pages/AuthPage";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import ProductsPage from "./pages/ProductsPage";
 import CreateProductPage from "./pages/CreateProductPage";
 import EditProductPage from "./pages/EditProductPage";
 import AdminPage from "./pages/AdminPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 import store from "./store";
 
@@ -24,14 +24,15 @@ function App() {
         <Header />
         <Container as="main" className="my-4">
           <Switch>
-            <Route path="/auth" component={Auth} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/checkout" component={Checkout} />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/cart" component={CartPage} />
+            <Route path="/profile" component={UserProfilePage} />
+            <Route path="/checkout" component={CheckoutPage} />
             <Route path="/create-product" component={CreateProductPage} />
             <Route path="/edit-product/:id" component={EditProductPage} />
-            <Route path="/products/:id" component={ProductPage} />
+            <Route path="/products/:id" component={ProductsPage} />
             <Route path="/admin" component={AdminPage} />
-            <Route exact path="/" component={Products} />
+            <Route exact path="/" component={ProductsPage} />
           </Switch>
         </Container>
         <Footer />
