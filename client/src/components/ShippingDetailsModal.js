9bef3f6043e34fd3cdde5151f32a237a43e1ce2d
Pedja Stojiljkovic <pedja1310@gmail.com>
2022-02-16 15:00:39 +0100

    shipping details moved to separate component with modal

--- client/src/components/ShippingDetailsModal.js
+++ client/src/components/ShippingDetailsModal.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Button, Form, Modal } from "react-bootstrap";
 import { updateUserAction } from "../store/actions/usersActions";
+import ShippingDetailsFrom from "./ShippingDetailsForm";
 
 const ShippingDetailsModal = ({
   showModal,
@@ -43,54 +44,11 @@ const ShippingDetailsModal = ({
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controledid="country" className="mt-3">
-            <Form.Label>Country</Form.Label>
-            <Form.Control
-              name="country"
-              type="text"
-              value={shippingDetails.country}
-              onChange={handleChange}
-              placeholder="Please enter country"
-            />
-          </Form.Group>
-          <Form.Group controledid="city" className="mt-3">
-            <Form.Label>City</Form.Label>
-            <Form.Control
-              name="city"
-              type="text"
-              value={shippingDetails.city}
-              onChange={handleChange}
-              placeholder="Please enter city"
-            />
-          </Form.Group>
-          <Form.Group controledid="postalCode" className="mt-3 ml-5">
-            <Form.Label>Postal Code</Form.Label>
-            <Form.Control
-              name="postalCode"
-              type="text"
-              value={shippingDetails.postalCode}
-              onChange={handleChange}
-              placeholder="Please enter postal code"
-            />
-          </Form.Group>
-          <Form.Group controledid="address" className="mt-3">
-            <Form.Label>Address</Form.Label>
-            <Form.Control
-              name="address"
-              type="text"
-              value={shippingDetails.address}
-              onChange={handleChange}
-              placeholder="Please enter address"
-            />
-          </Form.Group>
-          <Button type="button" className="mt-5">
-            Close
-          </Button>
-          <Button type="submit" className="ms-3 mt-5">
-            Submit
-          </Button>
-        </Form>
+        <ShippingDetailsFrom
+          shippingDetails={shippingDetails}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
       </Modal.Body>
     </Modal>
   );
