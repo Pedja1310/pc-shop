52732dcd48b9a241ccb675092db36484d3f43460
Pedja Stojiljkovic <pedja1310@gmail.com>
2022-05-26 19:09:40 +0200

    added user authentication before stripe paymant

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { authentication } = require("../middlewares/authMiddleware");
 
 const { newPaymentIntent } = require("../controllers/ordersController");
 
 const ordersRouter = express.Router();
 
-ordersRouter.route("/newPaymentIntent").post(newPaymentIntent);
+ordersRouter.route("/newPaymentIntent").post(authentication, newPaymentIntent);
 
 module.exports = ordersRouter;
